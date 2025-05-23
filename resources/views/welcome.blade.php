d94a91a78c87d8d64b93e6f5ec137348afcef483
Ahmed shamim <shaon.cse81@gmail.com>
2025-05-24 00:16:18 +0600

    fix: typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="w-full min-h-screen flex items-center justify-center bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500">
         <div class="text-center space-y-4">
             <h1 class="font-extrabold text-white tracking-tight md:text-6xl lg:text-7xl">
-                <span class="text-7xl bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-200">Effect caching strategies</span>
+                <span class="text-7xl bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-200">Effective caching strategies</span>
                 <span class="text-5xl block mt-1 bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-200">for Laravel developers</span>
             </h1>
             <p class="text-gray-200 text-xl max-w-3xl mx-auto">Learn to optimize your Laravel applications with powerful
